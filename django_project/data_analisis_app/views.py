9c57f2e3b8b76c924790e3ccb2ee0f12ba7b47c2
Ethan Martinez <ethanmartinez2503@gmail.com>
2024-07-14 04:16:53 -0500

    conexion del template home al backend

--- django_project/data_analisis_app/views.py
+++ django_project/data_analisis_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+'''
+creating webpage backend
+'''
 # Create your views here.
+
+def home(request):
+    #funcion que recibe al usuario
+    return render(request, 'home.html')
