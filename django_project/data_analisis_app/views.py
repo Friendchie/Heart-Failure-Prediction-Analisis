2125c5b432a17098b6391414a20736a792af5682
Ethan Martinez <ethanmartinez2503@gmail.com>
2024-07-16 21:13:10 -0500

    cambios minimos en dashboard
    visualizacion del csv para empezar a manipularlo

--- django_project/data_analisis_app/views.py
+++ django_project/data_analisis_app/views.py
@@ -1,4 +1,7 @@
+import os
+from django.conf import settings
 from django.shortcuts import render
+import pandas as pd
 
 '''
 creating webpage backend
@@ -8,3 +11,16 @@ creating webpage backend
 def home(request):
     #funcion que recibe al usuario
     return render(request, 'home.html')
+
+def dashboard(request):
+    # obtener direccion del csv
+    csv_path = os.path.join(settings.BASE_DIR, 'data_analisis_app', 'csv', 'heart_failure_clinical_records.csv')
+    # funcion que contiene el dash
+    csv_df = pd.read_csv(csv_path)
+
+    df_html = csv_df.to_html(classes='table table-striped')
+    
+    # Pasa el HTML del DataFrame al contexto
+    context = {'df_html': df_html}
+
+    return render(request, 'dashboard.html', context)
